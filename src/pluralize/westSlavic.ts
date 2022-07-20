53cbc61a9b951cef49a8a3d40e66faf4cc1ae579
Nando Vieira <me@fnando.com>
2022-07-20 15:55:52 -0700

    Apply code formatting.

--- src/pluralize/westSlavic.ts
+++ src/pluralize/westSlavic.ts
@@ -4,6 +4,7 @@ import { Pluralizer } from "../typing";
 export const westSlavic: Pluralizer = (_i18n: I18n, count: number) => {
   const few = [2, 3, 4];
   let key;
+
   if (count == 1) {
     key = "one";
   } else if (few.includes(count)) {
@@ -11,5 +12,6 @@ export const westSlavic: Pluralizer = (_i18n: I18n, count: number) => {
   } else {
     key = "other";
   }
+
   return [key];
 };
