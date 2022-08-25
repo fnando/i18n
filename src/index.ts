48cf0905427a78b691d940f6dec906dd79ca4817
Nando Vieira <me@fnando.com>
2022-08-25 07:42:18 -0300

    Export everything from typing.
    Close fnando/i18n-js#659.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { I18n } from "./I18n";
 export { Locales } from "./Locales";
 export { MissingTranslation } from "./MissingTranslation";
 export { Pluralization } from "./Pluralization";
+export * from "./typing";
